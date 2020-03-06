8fcb3c24cbdb8b6bcec919332e8c39ddd196b670
Sounryyy <justtrueserj@gmail.com>
2020-03-07 01:51:41 +0300

    Added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ module.exports = {
     mode: "development",
 
     module: {
+
         rules: [
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                  {
+                      loader: 'babel-loader'
+                  }
+              ]
+            },
+
             {
                 test: /\.(jpeg|jpg|png|ico)$/,
                 use: [
