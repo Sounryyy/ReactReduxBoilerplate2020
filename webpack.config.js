360d295ad3b28dfbf4b8d6446c363f88265ea52a
Sounryyy <justtrueserj@gmail.com>
2020-03-07 01:37:59 +0300

    Added supporting of fonts

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,25 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.png$/,
+                test: /\.(jpeg|jpg|png|ico)$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             outputPath: 'images',
                             name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name].[ext]'
                         }
                     }
                 ]
