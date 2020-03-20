1f13fe9dc9944c703e40278b37ea4c0c734d2dfc
Sounryyy <justtrueserj@gmail.com>
2020-03-20 21:52:12 +0300

    Added prod settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,86 @@
 const HtmlWebPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: "development",
+module.exports = (env = { mode: 'development' } ) => {
 
-    module: {
-        rules: [
+    const isProd = env.mode === 'production';
 
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-
-            {
-                test: /\.(css)$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
-            },
-
-            {
-                test: /\.(s[ca]ss)$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-            },
-
-            {
-                test: /\.(jpeg|jpg|png|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name]-[sha1:hash:7].[ext]'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(ttf|otf|eot|woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'fonts',
-                            name: '[name].[ext]'
-                        }
-                    }
-                ]
-            }
+    const getStyleLoaders = () => {
+        return [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
         ]
-    },
+    };
 
-    plugins: [
-        new HtmlWebPlugin({
-            template: 'public/index.html'
-        }),
-        new MiniCssExtractPlugin()
-    ],
+    const getPlugins = () => {
+        const plugins = [
+            new HtmlWebPlugin({
+                template: 'public/index.html'
+            }),
+        ];
 
-    devServer: {
-        open: true
+        if (isProd) {
+            plugins.push(new MiniCssExtractPlugin({
+                filename: 'main-[hash:8].css'
+            }))
+        }
+
+        return plugins
+    };
+
+    return {
+        mode: isProd ? 'production' : 'development',
+
+        module: {
+            rules: [
+
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                },
+
+                {
+                    test: /\.(css)$/,
+                    use: getStyleLoaders()
+                },
+
+                {
+                    test: /\.(s[ca]ss)$/,
+                    use: [...getStyleLoaders(), 'sass-loader']
+                },
+
+                {
+                    test: /\.(jpeg|jpg|png|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+
+        plugins: getPlugins(),
+
+        devServer: {
+            open: true
+        }
     }
 };
