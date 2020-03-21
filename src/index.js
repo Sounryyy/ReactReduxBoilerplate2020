3b0ba7417d8e15e1d9c221f91c96995c38bfc208
Sounryyy <justtrueserj@gmail.com>
2020-03-22 00:37:19 +0300

    Added linting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import css from './main.scss'
-
+import css from './main.scss';
 
 const App = () => <h1>Hello!</h1>;
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
